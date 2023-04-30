7f18e45f8e8847efd2c66731393d0be69011442c
YouceF-dz16 <y.aissat@gmail.com>
2023-04-30 18:14:07 +0200

    node 04 complete

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5050;
+const MY_NAME= process.env.MY_NAME;
+const MY_CITY= process.env.MY_CITY;
+const MY_LANGUAGE= process.env.MY_LANGUAGE;
+
+app.listen(PORT, (err)=>{
+    if(err) {
+        console.error("Oups, une erreur c'est produite")
+    }else{
+        console.log(`I am ${MY_NAME}, wilder in ${MY_CITY}, and I love ${MY_LANGUAGE}`);
+    }
+});
